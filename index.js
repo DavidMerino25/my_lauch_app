2813600d6f579eeca45382bf7b7e6b3a86daee2c
David2508 <deividrocan2508@gmail.com>
2022-04-20 09:23:23 -0500

    add: Modulos

---
+++ index.js
@@ -0,0 +1,8 @@
+// import MissionCommander from './app/missionCommander';
+const MissionCommander = require('./app/missionCommander');
+const Carlo = new MissionCommander("Carlo")
+console.log(Carlo.name)
+const Fer = new MissionCommander("Fer")
+console.log(Fer.name)
+const Romarpla = new MissionCommander("Romarpla")
+console.log(Romarpla.name)
